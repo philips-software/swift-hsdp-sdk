debb62032962048c954110bc66b06c57b7779553
Matthijs van Marion <matthijs.van.marion@philips.com>
2022-11-22 14:58:46 +0100

    revert version bump

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "swift-hsdp-sdk",
     platforms: [
-        .macOS(.v12), .iOS(.v15)
+        .macOS(.v10_15), .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
